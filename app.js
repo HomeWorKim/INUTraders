57474aa6246f50a8c3d1666bce0ca5fb61710ad4
HWK <befriends17@gmail.com>
2017-12-04 02:55:35 +0900

    Favicon test

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(favicon(path.join(__dirname, 'public','images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
